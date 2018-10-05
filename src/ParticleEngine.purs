b21ae963469f54543c890bdd6624fe9a0d63b87b
Jonas Buntinx <buntinxjonas@gmail.com>
2018-10-05 16:36:34 +0900

    Added rain, fireflies and fountain engine

--- src/ParticleEngine.purs
+++ src/ParticleEngine.purs
@@ -60,40 +60,137 @@ type Emitter =
   { age :: Number
   , state :: EmitterState }
 
-snowEngine :: ParticleEngine
-snowEngine =
+makeEngine :: ParticleEngine
+makeEngine =
   let
-    particlesPerSecond  = 200.0
-    particleDeathAge    = 4.0
+    particlesPerSecond  = 100.0
+    particleDeathAge    = 1.0
     emitterDeathAge     = 60.0
+    particleCount       = particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
 
   in
-    { positionBase:             { x: 0.0, y: 200.0, z: 0.0 }
-    , positionSpread:           { x: 500.0, y: 0.0, z: 500.0 }
+    { positionBase:             { x: 0.0, y: 0.0, z: 0.0 }
+    , positionSpread:           { x: 0.0, y: 0.0, z: 0.0 }
     , positionRadius:           0.0
-    , velocityBase:             { x: 0.0, y: (- 60.0), z: 0.0 }
-    , velocitySpread:           { x: 50.0, y: 20.0, z: 50.0 }
+    , velocityBase:             { x: 0.0, y: 0.0, z: 0.0 }
+    , velocitySpread:           { x: 0.0, y: 0.0, z: 0.0 }
     --, speedBase:                0.0
     --, speedSpread:              0.0
-    , accelerationBase:         { x: 0.0, y: (- 10.0), z: 0.0 }
+    , accelerationBase:         { x: 0.0, y: 0.0, z: 0.0 }
     , accelerationSpread:       { x: 0.0, y: 0.0, z: 0.0 }
     --, angleBase:                0.0
-    --, angleSpread:              720.0
+    --, angleSpread:              0.0
     --, angleVelocityBase:        0.0
-    --, angleVelocitySpread:      60.0
+    --, angleVelocitySpread:      0.0
     --, angleAccelerationBase:    0.0
     --, angleAccelerationSpread:  0.0
     --, sizeBase:                 0.0
     --, sizeSpread:               0.0
-    --, colorBase:                { x: 0.66, y: 1.0, z: 0.9 }
+    --, colorBase:                { x: 0.0, y: 1.0, z: 0.5 }
     --, colorSpread:              { x: 0.0, y: 0.0, z: 0.0 }
     --, opacityBase:              1.0
     --, opacitySpread:            0.0
     , particlesPerSecond
     , particleDeathAge
     , emitterDeathAge
-    , particleCount:            particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
+    , particleCount
     }
+
+snowEngine :: ParticleEngine
+snowEngine =
+  let
+    particlesPerSecond  = 200.0
+    particleDeathAge    = 4.0
+    emitterDeathAge     = 60.0
+    particleCount       = particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
+
+  in
+    makeEngine
+      { positionBase            = { x: 0.0, y: 200.0, z: 0.0 }
+      , positionSpread          = { x: 500.0, y: 0.0, z: 500.0 }
+      , velocityBase            = { x: 0.0, y: (- 60.0), z: 0.0 }
+      , velocitySpread          = { x: 50.0, y: 20.0, z: 50.0 }
+      , accelerationBase        = { x: 0.0, y: (- 10.0), z: 0.0 }
+      --, angleSpread             = 720.0
+      --, angleVelocitySpread     = 60.0
+      --, colorBase               = { x: 0.66, y: 1.0, z: 0.9 }
+      --, colorSpread             = { x: 0.0, y: 0.0, z: 0.0 }
+      , particlesPerSecond      = particlesPerSecond
+      , particleDeathAge        = particleDeathAge
+      , emitterDeathAge         = emitterDeathAge
+      , particleCount           = particleCount
+      }
+
+rainEngine :: ParticleEngine
+rainEngine =
+  let
+    particlesPerSecond  = 1000.0
+    particleDeathAge    = 1.0
+    emitterDeathAge     = 60.0
+    particleCount       = particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
+
+  in
+    makeEngine
+      { positionBase            = { x: 0.0, y: 200.0, z: 0.0 }
+      , positionSpread          = { x: 600.0, y: 0.0, z: 600.0 }
+      , velocityBase            = { x: 0.0, y: (- 400.0), z: 0.0 }
+      , velocitySpread          = { x: 10.0, y: 50.0, z: 10.0 }
+      , accelerationBase        = { x: 0.0, y: (- 10.0), z: 0.0 }
+      --, sizeBase                = 8.0
+      --, sizeSpread              = 4.0
+      --, colorBase               = { x: 0.66, y: 1.0, z: 0.7 }
+      --, colorSpread             = { x: 0.0, y: 0.0, z: 0.2 }
+      , particlesPerSecond      = particlesPerSecond
+      , particleDeathAge        = particleDeathAge
+      , emitterDeathAge         = emitterDeathAge
+      , particleCount           = particleCount
+      }
+
+firefliesEngine :: ParticleEngine
+firefliesEngine =
+  let
+    particlesPerSecond  = 20.0
+    particleDeathAge    = 6.0
+    emitterDeathAge     = 60.0
+    particleCount       = particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
+
+  in
+    makeEngine
+      { positionBase            = { x: 0.0, y: 100.0, z: 0.0 }
+      , positionSpread          = { x: 400.0, y: 200.0, z: 400.0 }
+      , velocitySpread          = { x: 60.0, y: 20.0, z: 60.0 }
+      --, sizeBase                = 30.0
+      --, sizeSpread              = 2.0
+      --, colorBase               = { x: 0.3, y: 1.0, z: 0.6 }
+      --, colorSpread             = { x: 0.3, y: 0.0, z: 0.0 }
+      , particlesPerSecond      = particlesPerSecond
+      , particleDeathAge        = particleDeathAge
+      , emitterDeathAge         = emitterDeathAge
+      , particleCount           = particleCount
+      }
+
+fountainEngine :: ParticleEngine
+fountainEngine =
+  let
+    particlesPerSecond  = 200.0
+    particleDeathAge    = 3.0
+    emitterDeathAge     = 60.0
+    particleCount       = particlesPerSecond * ( Math.min particleDeathAge emitterDeathAge )
+
+  in
+    makeEngine
+      { positionBase            = { x: 0.0, y: 5.0, z: 0.0 }
+      , positionSpread          = { x: 10.0, y: 0.0, z: 10.0 }
+      , velocityBase            = { x: 0.0, y: 160.0, z: 0.0 }
+      , velocitySpread          = { x: 100.0, y: 20.0, z: 100.0 }
+      , accelerationBase        = { x: 0.0, y: (- 100.0), z: 0.0 }
+      --, angleSpread             = 30.0
+      --, angleVelocitySpread     = 360.0 * 4.0
+      , particlesPerSecond      = particlesPerSecond
+      , particleDeathAge        = particleDeathAge
+      , emitterDeathAge         = emitterDeathAge
+      , particleCount           = particleCount
+      }
 
 makeParticle :: ParticleEngine -> Effect Particle
 makeParticle engine = do
